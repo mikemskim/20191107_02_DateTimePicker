7ba9031ca4710fca015276cf399eb4672e7ae155
mikemskim <mskim@steconsulting.co.kr>
2019-11-07 21:42:50 +0900

    Initial commit

---
+++ app/src/main/java/com/tj/a20191107_02_datetimepicker/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.tj.a20191107_02_datetimepicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
